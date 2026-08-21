71b4d74f8dc379518839a1403ad0969252830ab0
FredrikLoeknes <125686795+FredrikLoeknes@users.noreply.github.com>
2026-08-21 20:58:48 +0200

    Update cache name to 'tokt-v48'

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Handles push notifications and offline caching
 
 // Bump ved endring her → gammel cache slettes automatisk i 'activate'.
-const CACHE_NAME = 'tokt-v47';
+const CACHE_NAME = 'tokt-v48';
 const STATIC_ASSETS = ['/', '/index.html', '/terms.html'];
 
 // Install
